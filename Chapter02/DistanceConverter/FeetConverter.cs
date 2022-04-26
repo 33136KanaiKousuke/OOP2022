602ea38201a9d7983d82930b63198a1ad88d1da4
33136KanaiKosuke <33136@ojs.ac.jp>
2022-04-26 14:03:25 +0900

    定数raitoを定義

--- Chapter02/DistanceConverter/FeetConverter.cs
+++ Chapter02/DistanceConverter/FeetConverter.cs
@@ -7,15 +7,17 @@ using System.Threading.Tasks;
 namespace DistanceConverter {
     //フィートとメールの単位変換
     public static class FeetConverter {
-        
+        private const double ratio = 0.3048;//定数ratioを定義
+        //public static readonly double raito = 0.3048;//外部からもアクセスさせたい場合
+
         //メートルからフィートを求める(静的メソッド)
         public static double ToMeter(double meter) {
-            return meter / 0.3048;
+            return meter / ratio;
         }
 
         //フィートからメートルを求める(静的メソッド)
         public static double FromMeter(double feet) {
-            return feet * 0.3048;
+            return feet * ratio;
         }
     }
 }
