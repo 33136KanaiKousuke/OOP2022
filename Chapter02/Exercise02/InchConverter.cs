24283228a40086ad9027da2bb24179d535bc2e88
33136KanaiKosuke <33136@ojs.ac.jp>
2022-05-12 14:23:05 +0900

    問題2.2の完成

---
+++ Chapter02/Exercise02/InchConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    public static class InchConverter {
+        private const double ratio = 0.0254;//定数ratioを定義
+
+        //メートルからインチを求める(静的メソッド)
+        public static double ToMeter(double meter) {
+            return meter / ratio;
+        }
+
+        //インチからメートルを求める(静的メソッド)
+        public static double FromMeter(int inch) {
+              return inch * ratio;
+        }
+
+    }
+}
--- Chapter02/Exercise02/Program.cs
+++ Chapter02/Exercise02/Program.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Exercise02 {
     class Program {
-        static void Main(string[] args) {
-        }
+            //コマンドライン引数
+          static void Main(string[] args) {
+              if (args.Length >= 1 && args[0] == "-tom") {
+                    
+                  PrintToMeterList(1, 10);
+              }
+              else {
+                    
+                  PrintToInchList(1, 10);
+              }
+
+          }
+
+           //メートルからインチへの対応表を出力
+          private static void PrintToInchList(int start, int stop) {
+              for (int meter = 1; meter <= 10; meter++) {
+                  double inch = InchConverter.ToMeter(meter);
+                  Console.WriteLine("{1}in = {0:0.0000}m", inch, meter);
+              }
+          }
+
+           //インチからメートルへの対応表を出力
+          private static void PrintToMeterList(int start, int stop) {
+              for (int inch = 1; inch <= 10; inch++) {
+                  double meter = InchConverter.FromMeter(inch);
+                  Console.WriteLine("{0:0.0000}m = {1}in", meter, inch);
+              }
+          }
+
     }
 }
