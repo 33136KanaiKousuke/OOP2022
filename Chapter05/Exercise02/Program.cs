1c65376d8e97ff18bcfb2d466cf320b947230308
33136KanaiKosuke <33136@ojs.ac.jp>
2022-05-26 15:45:10 +0900

    問題5.2完成

--- Chapter05/Exercise02/Program.cs
+++ Chapter05/Exercise02/Program.cs
@@ -8,7 +8,13 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
             var str1 = Console.ReadLine();
-
+            int num;
+            if (int.TryParse(str1,out num)) {
+                var s1 = num.ToString("#,0");
+                Console.WriteLine(s1);
+            } else {
+                Console.WriteLine("変換失敗");
+            }
 
         }
     }
