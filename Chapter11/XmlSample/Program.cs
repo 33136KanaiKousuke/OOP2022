6bc0cca1528b9fcec2bff236984e93864e49e05f
33136KanaiKousuke <104185428+33136KanaiKousuke@users.noreply.github.com>
2022-06-28 13:35:59 +0900

    タイトルで表示

---
+++ Chapter11/XmlSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace XmlSample {
+    class Program {
+        static void Main(string[] args) {
+            using (var wc = new WebClient()) {
+
+                var stream = wc.OpenRead("https://news.yahoo.co.jp/rss/categories/business.xml");
+
+                var xdoc = XDocument.Load(stream);
+                var xNews = xdoc.Root.Descendants("item").Select(s=> s.Element("title"));
+
+                foreach (var data in xNews) {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
