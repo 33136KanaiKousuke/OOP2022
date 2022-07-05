01067ef1da386a9303b38deba41b0517ab0e5b0e
33136KanaiKousuke <104185428+33136KanaiKousuke@users.noreply.github.com>
2022-07-05 15:57:32 +0900

    逆シリアス化、シリアル化途中

---
+++ Chapter12/Section01/xmlSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml;
+
+namespace Section01 {
+    internal class xmlSerializer {
+        private Type type;
+
+        public xmlSerializer(Type type) {
+            this.type = type;
+        }
+
+        internal void Serialize(XmlWriter writeer, Novel novel) {
+            throw new NotImplementedException();
+        }
+
+        internal Novel Deserialize(XmlReader reader) {
+            throw new NotImplementedException();
+        }
+    }
+}
