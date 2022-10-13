e7a9789ebfd293d65c110b137d42e642a20096c0
33136KanaiKousuke <104185428+33136KanaiKousuke@users.noreply.github.com>
2022-10-13 14:08:36 +0900

    P024完成

---
+++ WPF/SampleApplication/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        MessageBox.Show("ボタンが押された")
+
+    End Sub
+
+    Private Sub okButton_Click(sender As Object, e As RoutedEventArgs) Handles okButton.Click
+
+    End Sub
+
+    Private Sub canselButton_Click(sender As Object, e As RoutedEventArgs) Handles canselButton.Click
+
+    End Sub
+End Class
