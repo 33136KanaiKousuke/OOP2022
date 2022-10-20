392f91aa5a513661c7c675432062eee010a6fa1d
33136KanaiKousuke <104185428+33136KanaiKousuke@users.noreply.github.com>
2022-10-20 11:19:02 +0900

    SampleSlider終了

--- WPF/NumberGame/MainWindow.xaml.cs
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -20,27 +20,36 @@ namespace NumberGame {
     public partial class MainWindow : Window {
         private Random rnd = new Random();
 
-        private int randomNumber;
+        private int randomNumber;//正解ナンバー
 
         public MainWindow() {
             InitializeComponent();
-            randomNumber = rnd.Next(1, 25);
+            randomNumber = rnd.Next(1, 25);//正解ナンバーの設定
+            //randomNumber = rnd.Next(25) + 1;//正解ナンバーの設定
         }
-        
+
         private void Button_Click(object sender, RoutedEventArgs e) {
             
             Button bt = (Button)sender;
-
+            
             if (int.Parse((string)bt.Content) == randomNumber) {
                 infoDisp.Text = "正解";
+                bt.Background = Brushes.Green;
             }
             else if (int.Parse((string)bt.Content) > randomNumber) {
                 infoDisp.Text = "もっと小さい";
+                bt.Background = Brushes.Red;
             }
             else {
                 infoDisp.Text = "もっと大きい";
+                bt.Background = Brushes.Blue;
             }
             
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            infoDisp.Text = "ゲームスタート";
+            this.Title = randomNumber.ToString();
         }
     }
 }
