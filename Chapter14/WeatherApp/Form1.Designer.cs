eb11a435e757ec94f66991d20416f639e9812e26
33136KanaiKousuke <104185428+33136KanaiKousuke@users.noreply.github.com>
2022-11-17 15:58:01 +0900

    マーク途中

--- Chapter14/WeatherApp/Form1.Designer.cs
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -31,19 +31,29 @@ namespace WeatherApp {
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.pbTodayWether = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbPublishingOffice = new System.Windows.Forms.TextBox();
+            this.tbreportDatetime = new System.Windows.Forms.TextBox();
+            this.tbTomorrow = new System.Windows.Forms.TextBox();
+            this.tbDayAfterTomorrow = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbToday = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTodayWether)).BeginInit();
             this.SuspendLayout();
             // 
             // tbWeatherInfo
             // 
-            this.tbWeatherInfo.Location = new System.Drawing.Point(135, 275);
+            this.tbWeatherInfo.Location = new System.Drawing.Point(164, 361);
             this.tbWeatherInfo.Multiline = true;
             this.tbWeatherInfo.Name = "tbWeatherInfo";
-            this.tbWeatherInfo.Size = new System.Drawing.Size(572, 143);
+            this.tbWeatherInfo.Size = new System.Drawing.Size(364, 186);
             this.tbWeatherInfo.TabIndex = 0;
             // 
             // btWeatherGet
             // 
-            this.btWeatherGet.Location = new System.Drawing.Point(36, 462);
+            this.btWeatherGet.Location = new System.Drawing.Point(348, 85);
             this.btWeatherGet.Name = "btWeatherGet";
             this.btWeatherGet.Size = new System.Drawing.Size(75, 43);
             this.btWeatherGet.TabIndex = 1;
@@ -54,16 +64,17 @@ namespace WeatherApp {
             // cmArea
             // 
             this.cmArea.FormattingEnabled = true;
-            this.cmArea.Location = new System.Drawing.Point(135, 181);
+            this.cmArea.Location = new System.Drawing.Point(170, 93);
             this.cmArea.Name = "cmArea";
             this.cmArea.Size = new System.Drawing.Size(141, 20);
             this.cmArea.TabIndex = 2;
+            this.cmArea.SelectedIndexChanged += new System.EventHandler(this.cmArea_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(23, 275);
+            this.label1.Location = new System.Drawing.Point(23, 361);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(106, 24);
             this.label1.TabIndex = 3;
@@ -73,7 +84,7 @@ namespace WeatherApp {
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(23, 177);
+            this.label2.Location = new System.Drawing.Point(23, 85);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(106, 24);
             this.label2.TabIndex = 3;
@@ -93,19 +104,101 @@ namespace WeatherApp {
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label4.Location = new System.Drawing.Point(23, 96);
+            this.label4.Location = new System.Drawing.Point(388, 32);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(106, 24);
             this.label4.TabIndex = 3;
             this.label4.Text = "報告日時";
             // 
+            // pbTodayWether
+            // 
+            this.pbTodayWether.Location = new System.Drawing.Point(485, 106);
+            this.pbTodayWether.Name = "pbTodayWether";
+            this.pbTodayWether.Size = new System.Drawing.Size(186, 105);
+            this.pbTodayWether.TabIndex = 4;
+            this.pbTodayWether.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(23, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 24);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "明日の天気";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(23, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 24);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "明後日の天気";
+            // 
+            // tbPublishingOffice
+            // 
+            this.tbPublishingOffice.Location = new System.Drawing.Point(170, 37);
+            this.tbPublishingOffice.Name = "tbPublishingOffice";
+            this.tbPublishingOffice.Size = new System.Drawing.Size(141, 19);
+            this.tbPublishingOffice.TabIndex = 5;
+            // 
+            // tbreportDatetime
+            // 
+            this.tbreportDatetime.Location = new System.Drawing.Point(515, 34);
+            this.tbreportDatetime.Name = "tbreportDatetime";
+            this.tbreportDatetime.Size = new System.Drawing.Size(141, 19);
+            this.tbreportDatetime.TabIndex = 5;
+            // 
+            // tbTomorrow
+            // 
+            this.tbTomorrow.Location = new System.Drawing.Point(179, 226);
+            this.tbTomorrow.Name = "tbTomorrow";
+            this.tbTomorrow.Size = new System.Drawing.Size(185, 19);
+            this.tbTomorrow.TabIndex = 6;
+            // 
+            // tbDayAfterTomorrow
+            // 
+            this.tbDayAfterTomorrow.Location = new System.Drawing.Point(179, 313);
+            this.tbDayAfterTomorrow.Name = "tbDayAfterTomorrow";
+            this.tbDayAfterTomorrow.Size = new System.Drawing.Size(185, 19);
+            this.tbDayAfterTomorrow.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(23, 153);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 24);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "今日の天気";
+            // 
+            // tbToday
+            // 
+            this.tbToday.Location = new System.Drawing.Point(179, 158);
+            this.tbToday.Name = "tbToday";
+            this.tbToday.Size = new System.Drawing.Size(185, 19);
+            this.tbToday.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(852, 614);
+            this.ClientSize = new System.Drawing.Size(827, 579);
+            this.Controls.Add(this.tbDayAfterTomorrow);
+            this.Controls.Add(this.tbToday);
+            this.Controls.Add(this.tbTomorrow);
+            this.Controls.Add(this.tbreportDatetime);
+            this.Controls.Add(this.tbPublishingOffice);
+            this.Controls.Add(this.pbTodayWether);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cmArea);
@@ -113,6 +206,8 @@ namespace WeatherApp {
             this.Controls.Add(this.tbWeatherInfo);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTodayWether)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,6 +222,15 @@ namespace WeatherApp {
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pbTodayWether;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbPublishingOffice;
+        private System.Windows.Forms.TextBox tbreportDatetime;
+        private System.Windows.Forms.TextBox tbTomorrow;
+        private System.Windows.Forms.TextBox tbDayAfterTomorrow;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbToday;
     }
 }
 
--- Chapter14/WeatherApp/Form1.cs
+++ Chapter14/WeatherApp/Form1.cs
@@ -13,35 +13,281 @@ using System.IO;
 
 namespace WeatherApp {
     public partial class Form1 : Form {
-        //private IEnumerable<Area> _area;
-
+        string areacode;
+        string weathercode;
+        string tweathercode;
+        public static readonly string url = "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/";
+        //"https://www.jma.go.jp/bosai/forecast/img/100.png";
+        public static readonly string url2 = "https://www.jma.go.jp/bosai/forecast/data/forecast/";
         public Form1() {
             InitializeComponent();
         }
 
-        //public static IEnumerable<Area> ReadSales(string filePath) {
-        //    List<Area> areas = new List<Area>();
-        //    string[] lines = File.ReadAllLines(filePath);
-        //    foreach (string line in lines) {
-        //        string[] items = line.Split(',');
-        //        Area area = new Area {
-        //            area = items[0],
-        //            areacode = items[1]
-
-        //        };
-        //        areas.Add(area);
-        //    }
-        //    return areas;
-        //}
+        private void Form1_Load(object sender, EventArgs e) {
+            cmArea.Items.Add("宗谷地方");
+            cmArea.Items.Add("上川・留萌地方");
+            cmArea.Items.Add("網走・北見・紋別地方");
+            cmArea.Items.Add("十勝地方");
+            cmArea.Items.Add("釧路・根室地方");
+            cmArea.Items.Add("胆振・日高地方");
+            cmArea.Items.Add("石狩・空知・後志地方");
+            cmArea.Items.Add("渡島・檜山地方");
+            cmArea.Items.Add("青森県");
+            cmArea.Items.Add("岩手県");
+            cmArea.Items.Add("宮城県");
+            cmArea.Items.Add("秋田県");
+            cmArea.Items.Add("山形県");
+            cmArea.Items.Add("福島県");
+            cmArea.Items.Add("茨城県");
+            cmArea.Items.Add("栃木県");
+            cmArea.Items.Add("群馬県");
+            cmArea.Items.Add("埼玉県");
+            cmArea.Items.Add("千葉県");
+            cmArea.Items.Add("東京都");
+            cmArea.Items.Add("神奈川県");
+            cmArea.Items.Add("山梨県");
+            cmArea.Items.Add("長野県");
+            cmArea.Items.Add("岐阜県");
+            cmArea.Items.Add("静岡県");
+            cmArea.Items.Add("愛知県");
+            cmArea.Items.Add("三重県");
+            cmArea.Items.Add("新潟県");
+            cmArea.Items.Add("富山県");
+            cmArea.Items.Add("石川県");
+            cmArea.Items.Add("福井県");
+            cmArea.Items.Add("滋賀県");
+            cmArea.Items.Add("京都府");
+            cmArea.Items.Add("大阪府");
+            cmArea.Items.Add("兵庫県");
+            cmArea.Items.Add("奈良県");
+            cmArea.Items.Add("和歌山県");
+            cmArea.Items.Add("鳥取県");
+            cmArea.Items.Add("島根県");
+            cmArea.Items.Add("岡山県");
+            cmArea.Items.Add("広島県");
+            cmArea.Items.Add("徳島県");
+            cmArea.Items.Add("香川県");
+            cmArea.Items.Add("愛媛県");
+            cmArea.Items.Add("高知県");
+            cmArea.Items.Add("山口県");
+            cmArea.Items.Add("福岡県");
+            cmArea.Items.Add("佐賀県");
+            cmArea.Items.Add("長崎県");
+            cmArea.Items.Add("熊本県");
+            cmArea.Items.Add("大分県");
+            cmArea.Items.Add("宮崎県");
+            cmArea.Items.Add("奄美地方");
+            cmArea.Items.Add("鹿児島県（奄美地方除く）");
+            cmArea.Items.Add("沖縄本島地方");
+            cmArea.Items.Add("大東島地方");
+            cmArea.Items.Add("宮古島地方");
+            cmArea.Items.Add("八重山地方");
+        }
 
         private void btWeatherGet_Click(object sender, EventArgs e) {
             var wc = new WebClient() {
                 Encoding = Encoding.UTF8
             };
 
-            var dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/100000.json");
+            switch (cmArea.SelectedIndex) 
+            {
+                case 0:
+                    areacode = "011000";
+                    break;
+                case 1:
+                    areacode = "012000";
+                    break;
+                case 2:
+                    areacode = "013000";
+                    break;
+                case 3:
+                    areacode = "014030";
+                    break;
+                case 4:
+                    areacode = "014100";
+                    break;
+                case 5:
+                    areacode = "015000";
+                    break;
+                case 6:
+                    areacode = "016000";
+                    break;
+                case 7:
+                    areacode = "017000";
+                    break;
+                case 8:
+                    areacode = "020000";
+                    break;
+                case 9:
+                    areacode = "030000";
+                    break;
+                case 10:
+                    areacode = "040000";
+                    break;
+                case 11:
+                    areacode = "050000";
+                    break;
+                case 12:
+                    areacode = "060000";
+                    break;
+                case 13:
+                    areacode = "070000";
+                    break;
+                case 14:
+                    areacode = "080000";
+                    break;
+                case 15:
+                    areacode = "090000";
+                    break;
+                case 16:
+                    areacode = "100000";
+                    break;
+                case 17:
+                    areacode = "110000";
+                    break;
+                case 18:
+                    areacode = "120000";
+                    break;
+                case 19:
+                    areacode = "130000";
+                    break;
+                case 20:
+                    areacode = "140000";
+                    break;
+                case 21:
+                    areacode = "190000";
+                    break;
+                case 22:
+                    areacode = "200000";
+                    break;
+                case 23:
+                    areacode = "210000";
+                    break;
+                case 24:
+                    areacode = "220000";
+                    break;
+                case 25:
+                    areacode = "230000";
+                    break;
+                case 26:
+                    areacode = "240000";
+                    break;
+                case 27:
+                    areacode = "150000";
+                    break;
+                case 28:
+                    areacode = "160000";
+                    break;
+                case 29:
+                    areacode = "170000";
+                    break;
+                case 30:
+                    areacode = "180000";
+                    break;
+                case 31:
+                    areacode = "250000";
+                    break;
+                case 32:
+                    areacode = "260000";
+                    break;
+                case 33:
+                    areacode = "270000";
+                    break;
+                case 34:
+                    areacode = "280000";
+                    break;
+                case 35:
+                    areacode = "290000";
+                    break;
+                case 36:
+                    areacode = "300000";
+                    break;
+                case 37:
+                    areacode = "310000";
+                    break;
+                case 38:
+                    areacode = "320000";
+                    break;
+                case 39:
+                    areacode = "330000";
+                    break;
+                case 40:
+                    areacode = "340000";
+                    break;
+                case 41:
+                    areacode = "360000";
+                    break;
+                case 42:
+                    areacode = "370000";
+                    break;
+                case 43:
+                    areacode = "380000";
+                    break;
+                case 44:
+                    areacode = "390000";
+                    break;
+                case 45:
+                    areacode = "350000";
+                    break;
+                case 46:
+                    areacode = "400000";
+                    break;
+                case 47:
+                    areacode = "410000";
+                    break;
+                case 48:
+                    areacode = "420000";
+                    break;
+                case 49:
+                    areacode = "430000";
+                    break;
+                case 50:
+                    areacode = "440000";
+                    break;
+                case 51:
+                    areacode = "450000";
+                    break;
+                case 52:
+                    areacode = "460040";
+                    break;
+                case 53:
+                    areacode = "460100";
+                    break;
+                case 54:
+                    areacode = "471000";
+                    break;
+                case 55:
+                    areacode = "472000";
+                    break;
+                case 56:
+                    areacode = "473000";
+                    break;
+                case 57:
+                    areacode = "474000";
+                    break;
+            }
+
+            //天気概況
+            var dString = wc.DownloadString(url + areacode +".json");
             var json = JsonConvert.DeserializeObject<Rootobject>(dString);
+
+            //天気
+            var cString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/forecast/"+ areacode + ".json");
+            var cjson = JsonConvert.DeserializeObject<Class1[]>(cString);
+
+            //var bString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/img/100.png");
+            //var bjson = JsonConvert.DeserializeObject<Class1[]>(bString);
+
+            tbPublishingOffice.Text = json.publishingOffice;
+            tbreportDatetime.Text = json.reportDatetime.ToString();
             tbWeatherInfo.Text = json.text;
+            tbToday.Text = cjson[0].timeSeries[0].areas[0].weathers[0];
+            tbTomorrow.Text = cjson[0].timeSeries[0].areas[0].weathers[1];
+            tbDayAfterTomorrow.Text = cjson[0].timeSeries[0].areas[0].weathers[2];
+        }
+
+        private void cmArea_SelectedIndexChanged(object sender, EventArgs e) {
+
         }
     }
 }
